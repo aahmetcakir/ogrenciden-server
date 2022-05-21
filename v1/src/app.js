f477533fe4962a3580abd1737d2680e735f8fac8
ahmet <ca.ahmetcakir@gmail.com>
2022-05-21 23:12:05 +0300

    increase limit

--- v1/src/app.js
+++ v1/src/app.js
@@ -7,7 +7,8 @@ const app = express();
 const { ProductsRoutes, UserRoutes } = require("./api-routes");
 config();
 loader();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(helmet());
 app.use(cors());
 
