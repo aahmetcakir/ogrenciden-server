3eeb56f6ef4274a187cd2caa7984b908c55acff2
ahmet <ca.ahmetcakir@gmail.com>
2022-05-21 16:00:38 +0300

    deploy 8

--- v1/src/app.js
+++ v1/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 const config = require("./config");
 const loader = require("./loaders");
 const app = express();
@@ -8,6 +9,7 @@ config();
 loader();
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("sunucu ayakta");
