4ca90680e2e7e0d859db67b6f038bbf849a8551c
ahmet <ca.ahmetcakir@gmail.com>
2022-08-27 15:57:17 +0300

    cors

--- v1/src/app.js
+++ v1/src/app.js
@@ -7,10 +7,19 @@ const app = express();
 const { ProductsRoutes, UserRoutes } = require("./api-routes");
 config();
 loader();
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(helmet());
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.ogrenciden.co/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("sunucu ayakta");
